1149d6751c1590e2fb2b485cec68714d4aa7316d
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-02-27 14:57:41 +0500

    first commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Izaan Saleem on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
